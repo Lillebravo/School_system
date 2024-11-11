8ebe871b26292f3933da492a62813289f5325c26
Jerry <you@example.com>
2024-11-11 11:57:08 +0100

    added a person class and teacher/student that build upon the person

---
+++ modules/student.js
@@ -0,0 +1,32 @@
+export class Student extends Person {
+    #age;
+    #gender;
+  
+    constructor(name, age, gender) {
+      super(name);
+      this.#age = age;
+      this.#gender = gender;
+    }
+  
+    get age() {
+      return this.#age;
+    }
+  
+    get gender() {
+      return this.#gender;
+    }
+  
+    getGrades() {
+      const allGrades = {};
+      this.subjects.forEach(subject => {
+        const gradeInfo = subject.getStudentGrade(this.name);
+        if (gradeInfo) {
+          allGrades[subject.name] = {
+            ...gradeInfo,
+            points: subject.points
+          };
+        }
+      });
+      return allGrades;
+    }
+  }
---
+++ modules/teacher.js
@@ -0,0 +1,15 @@
+export class Teacher extends Person {
+    gradeStudent(student, subject, grade) {
+      if (!this.subjects.includes(subject)) {
+        return `${this.name} is not authorized to grade ${subject.name}`;
+      }
+      if (!student.subjects.includes(subject)) {
+        return `${student.name} is not enrolled in ${subject.name}`;
+      }
+      if (!Grade.isValid(grade)) {
+        return `Invalid grade: ${grade}`;
+      }
+  
+      return subject.setGrade(student.name, grade, this.name);
+    }
+  }
